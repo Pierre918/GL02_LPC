42553c64766d05004fa32c522fc6cddaf0ec039e
Lucile Pereira <lucile.pereira@utt.com>
2024-11-08 18:02:14 +0100

    retour à l'état initial

--- command.js
+++
@@ -1,9 +0,0 @@
-var Command = {
-check: function(){
-console.log("Command Ready !")
-console.log("Magenta level checked".magenta)
-console.log("cyan level checked".cyan) 
-return true
-}
-}
-module.exports = Command
