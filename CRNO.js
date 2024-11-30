ff0e5d5e8fd9d3fc7738d3a8a99128dba549d0d3
LucilePereira <123165101+LucilePereira@users.noreply.github.com>
2024-11-30 21:35:04 +0100

    Mise à jour de CRNO.js
    Remplacement des mentions de POI par CRNO

--- CRNO.js
+++ CRNO.js
@@ -1,13 +1,15 @@
-var CRNO = function(ue, j, hd, hf, s, c){
+var CRNO = function(ue, t, c, j, hd, hf, grp, s){
     this.ue = ue;
+	this.type = t;
+	this.capacite = c;
 	this.jour = j;
 	this.hdeb = hd;
 	this.hfin = hf;
+	this.groupe = grp;
 	this.salle = s;
-	this.capacite = c;
 }
 	
-POI.prototype.memeHoraire = function(crno){
+CRNO.prototype.memeHoraire = function(crno){
 	if (this.jour === crno.jour){
 	   if (this.hdeb === crno.hdeb){
 	       return this.hfin === crno.hfin;
@@ -17,24 +19,24 @@ POI.prototype.memeHoraire = function(crno){
 
 };
 
-POI.prototype.memeSalle = function(crno){
+CRNO.prototype.memeSalle = function(crno){
 	return this.salle === crno.salle;
 };
 
-POI.prototype.memeCapacite = function(crno){
+CRNO.prototype.memeCapacite = function(crno){
 	return this.capacite === crno.capacite;
 };
 
-POI.prototype.memeUE = function(crno){
+CRNO.prototype.memeUE = function(crno){
 	return this.ue === crno.ue;
 };
 
-POI.prototype.plusTard = function(crno){
+CRNO.prototype.plusTard = function(crno){
 	return this.jour === crno.jour && this.hfin > crno.hfin;
 };
 
-POI.prototype.plusGrand = function(crno){
+CRNO.prototype.plusGrand = function(crno){
 	return this.capacite > crno.capacite;
 };
 
-module.exports = POI;
+module.exports = CRNO;
