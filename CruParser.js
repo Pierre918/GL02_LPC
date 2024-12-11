5d3e98656ba9990c0ccd92556d34a835b4d8139b
LucilePereira <123165101+LucilePereira@users.noreply.github.com>
2024-12-12 00:51:10 +0100

    Mise à jour de CruParser.js
    Conformation aux règles de l’ABNF définies dans le cahier des charges

--- CruParser.js
+++ CruParser.js
@@ -3,12 +3,10 @@ var CRNO = require('./CRNO');
 
 // CruParser
 
-var CruParser = function(sTokenize, sParsedSymb){
+var CruParser = function(){
 	// The list of CRNO parsed from the input file.
 	this.parsedCRNO = [];
 	this.symb = ["+","1","P","H","F","S","//","Page"];
-	this.showTokenize = sTokenize;
-	this.showParsedSymbols = sParsedSymb;
 	this.errorCount = 0;
 }
 
@@ -18,7 +16,7 @@ var CruParser = function(sTokenize, sParsedSymb){
 // <eol> = CRLF
 CruParser.prototype.tokenize = function(data){
 	var toBeRemoved = /(\r\n|=|,)/;
-	var splitter = /(\r\n|=|,|\+|Page|F|\/\/)/;
+	var splitter = /(\r\n|=|,|\+|Page|\/\/)/;
 	data = data.split(splitter);
 	data = data.filter((val, idx) => !val.match(toBeRemoved) && val !== '');
 	return data;
@@ -27,9 +25,6 @@ CruParser.prototype.tokenize = function(data){
 // parse : analyze data by calling the first non terminal rule of the grammar
 CruParser.prototype.parse = function(data){
 	var tData = this.tokenize(data);
-	if(this.showTokenize){
-		console.log(tData);
-	}
 	this.listUE(tData);
 }
 
@@ -37,20 +32,17 @@ CruParser.prototype.parse = function(data){
 
 CruParser.prototype.errMsg = function(msg, input){
 	this.errorCount++;
-	if(input !== undefined){
-	   console.log("Erreur de parsing ! sur " + input + " -- msg : " + msg);
+	if(input !== undefined && input !== " " && input !== ""){
+	   console.log("Erreur de parsing sur " + input + " -- msg : " + msg);
 	}
 	else{
-	   console.log("Erreur de parsing ! -- msg : " + msg);
+	   console.log("Erreur de parsing -- msg : " + msg);
 	}
 }
 
 // Read and return a symbol from input
 CruParser.prototype.next = function(input){
 	var curS = input.shift();
-	if(this.showParsedSymbols){
-		console.log(curS);
-	}
 	return curS;
 }
 
@@ -59,7 +51,7 @@ CruParser.prototype.accept = function(s){
 	var idx = this.symb.indexOf(s);
 	// index 0 exists
 	if(idx === -1){
-		this.errMsg("symbole " + s + " inconnu", [" "]);
+		this.errMsg("symbole " + s + " inconnu", "");
 		return false;
 	}
 
@@ -76,11 +68,12 @@ CruParser.prototype.check = function(s, input){
 
 // expect : expect the next symbol to be s.
 CruParser.prototype.expect = function(s, input){
-	if(s == this.next(input)){
+    let next = this.next(input);
+	if(s == next){
 		//console.log("Reconnu ! " + s);
 		return true;
 	}else{
-		this.errMsg("le symbole " + s + " ne correspond pas à ", input);
+		this.errMsg(next + " ne correspond pas au symbole " + s, next);
 	}
 	return false;
 }
@@ -88,22 +81,40 @@ CruParser.prototype.expect = function(s, input){
 
 // Parser rules
 
-// <liste_ue> = <txtDebut> *(<ue>) "Page" *VCHAR
+// <liste_ue> = <texte_debut> *(<nom_ue>) "Page" *VCHAR
 CruParser.prototype.listUE = function(input){
-	let endOfTxt = input.indexOf("UVUV");
-	let i = 0;
-	while(i<=endOfTxt || input[0]!=='+'){
-	   input.shift();
-	  i++;
-	}
+    this.texteDebut(input);
 	this.listCrno(input);
-	this.expect("Page", input);
+}
+
+// <texte_debut> = “EDT.CRU - Test\n
+//                 V�rifier ce qui se passe en cas de s�ance li�e avec salles multiples :\n
+//                 Comportement souhait� : Si 2 seances li�e avec deux salles chacunes\n
+//                 Seance 1 : salle 1 et 2 - Seance 2 : salle 3 et 4\n
+//                 on doit avoir :\n
+//                 +UVUV\n
+//                 Seance 1 S=1 / Seance 2 S=3\n
+//                 Seance 1 S=2 / Seance2 S=4\n"
+CruParser.prototype.texteDebut = function(input){
+    this.expect("EDT.CRU - Test", input);
+    this.expect("V�rifier ce qui se passe en cas de s�ance li�e avec salles multiples :", input);
+    this.expect("Comportement souhait� : Si 2 seances li�e avec deux salles chacunes", input);
+    this.expect("Seance 1 : salle 1 et 2 - Seance 2 : salle 3 et 4", input);
+    this.expect("on doit avoir :", input);
+    this.expect("+", input);
+    this.expect("UVUV", input);
+    this.expect("Seance 1 S", input);
+    this.expect("1 / Seance 2 S", input);
+    this.expect("3", input);
+    this.expect("Seance 1 S", input);
+    this.expect("2 / Seance2 S", input);
+    this.expect("4", input);
 }
 
 // <liste_crno> = *(<crno>) "Page" *VCHAR
 CruParser.prototype.listCrno = function(input){
     if(this.check("+", input)){
-		var ue = this.ue(input);
+		var ue = this.nom_ue(input);
 		this.crno(input, ue);
 		if(input.length > 0){
 			this.listCrno(input);
@@ -114,11 +125,11 @@ CruParser.prototype.listCrno = function(input){
 	}
 }
 
-// <crno> = "1" ”,” <body> “//”
+// <crno> = "1" ”,” <ligne_description> “//”
 CruParser.prototype.crno = function(input, ue){
 	if(this.check("1", input)){
 		this.expect("1", input);
-		var args = this.body(input);
+		var args = this.ligne_description(input);
 		var c = new CRNO(ue, args.typ, args.cap, args.jor, args.hdeb, args.hfin, args.grp, args.sal);
 		this.expect("//", input);
 		this.parsedCRNO.push(c);
@@ -132,40 +143,39 @@ CruParser.prototype.crno = function(input, ue){
 
 }
 
-// <body> = <type> “," <capacite> ”," <creneau> “,” <groupe> “,” <salle>
-CruParser.prototype.body = function(input){
-	var typ = this.type(input);
+// <ligne_description> = <type> “," <capacite> ”," <creneau> “,” <groupe> “,” <salle>
+CruParser.prototype.ligne_description = function(input){
+	var typ = this.variable(input);
 	var cap = this.capacite(input);
 	var crn = this.creneau(input);
-	var grp = this.groupe(input);
+	var grp = this.variable(input);
 	var sal = this.salle(input);
 	return { typ: typ, cap: cap, jor: crn.jor, hdeb: crn.hdeb, hfin : crn.hfin, grp: grp, sal: sal };
 }
 
-// <ue> = "+" 2VCHAR 2DIGIT
-CruParser.prototype.ue = function(input){
-	this.expect("+",input)
+// <nom_ue> = "+" 2*VCHAR 
+CruParser.prototype.nom_ue = function(input){
+	this.expect("+",input);
 	var curS = this.next(input);
-	if(matched = curS.match(/([A-Z][A-Z]\d\d)/)){
+	if(matched = curS.match(/([a-zA-Z0-9][a-zA-Z0-9]+)/)){
 		return matched[1];
 	}else{
 		this.errMsg("UE non valide", curS);
 	}
 }
 
-// <type> = "C"/"D"/"T" DIGIT
-CruParser.prototype.type = function(input){
-	var curS = this.next(input);
-	if(matched = curS.match(/(C|D|T)(\d+)/)){
+// <variable> = VCHAR DIGIT
+CruParser.prototype.variable = function(input){
+	var curS = this.next(input);	
+	if(matched = curS.match(/[a-zA-Z0-9](\d)/)){
 		return matched[0];
 	}else{
-		this.errMsg("Type non valide", curS);
+		this.errMsg("Variable non valide", curS);
 	}
 }
 
 // <capacite> = "P=" 1*3DIGIT
-CruParser.prototype.capacite = function (input){
-	this.expect("P", input);
+CruParser.prototype.capacite = function (input){	this.expect("P", input);
 	var curS = this.next(input);
 	if(matched = curS.match(/[0-9][0-9]?[0-9]?/)){
 		return matched[0];
@@ -174,11 +184,11 @@ CruParser.prototype.capacite = function (input){
 	}
 }	
 
-// <creneau> =  "L"/"MA"/"ME"/"J"/"V"/"S"/"D" WSP <heureDebut> "-" <heureFin>
+// <creneau> =  1*2CHAR WSP <heureDebut> "-" <heureFin>
 CruParser.prototype.creneau = function(input){
 	this.expect("H",input)
 	var curS = this.next(input);
-	if(matched = curS.match(/(L|MA|ME|J|V|S|D) (\d(\d)?:\d\d)-(\d(\d)?:\d\d)/)){
+	if(matched = curS.match(/([a-zA-Z][a-zA-Z]?) (\d(\d)?:\d\d)-(\d(\d)?:\d\d)/)){
 		return { jor: matched[1], hdeb: matched[2], hfin: matched[4] };
 	}else{
 		this.errMsg("Créneau non valide", curS);
@@ -186,22 +196,11 @@ CruParser.prototype.creneau = function(input){
 	}
 }
 
-// <groupe> = "F" "1"/"2"/"3"/"4"/"5"/"6"/"7"/"8"/"9"/
-CruParser.prototype.groupe = function (input){
-	this.expect("F", input);
-	var curS = this.next(input);
-	if(matched = curS.match(/[1-9]/)){
-		return matched[0];
-	}else{
-		this.errMsg("Groupe non valide", curS);
-	}
-}	
-
-// <salle> = VCHAR 3DIGIT
+// <salle> = 4VCHAR
 CruParser.prototype.salle = function (input){
 	this.expect("S", input);
 	var curS = this.next(input);
-	if(matched = curS.match(/[A-Z]\d\d\d/)){
+	if(matched = curS.match(/[a-zA-Z0-9]{4}/)){
 		return matched[0];
 	}else{
 		this.errMsg("Salle non valide", curS);
